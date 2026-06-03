0fc543a97c55aa17b67a271eeca658907f33b4f1
GowthamanM <gowthmanm585@gmail.com>
2026-06-03 21:47:11 +0530

    Added : structure directory

---
+++ structure/struct.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+int main(){
+	struct part{
+		int number;
+		int array[SIZE];
+	}arr[SIZE];
+
+	arr[0].number = 10;
+	arr[0].array[] = {1,2,3,4,5,6,7,8,9};
+
+
+	return 0;
+}
+
